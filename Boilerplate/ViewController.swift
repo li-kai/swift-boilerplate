fbd740c230a24b70367194e785b65c727f9e5c6c
Li Kai <falconets25601@gmail.com>
2017-02-28 16:34:16 +0800

    Initial Commit

---
+++ Boilerplate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Boilerplate
+//
+//  Created by Li Kai on 28/2/17.
+//  Copyright © 2017 nus.cs3217.li-kai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
